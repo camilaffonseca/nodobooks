86ac8ecd7956a1a1dc279df7cdf81260d284131a
camilaffonseca <camilafonseca.ang7@gmail.com>
2020-11-11 15:58:02 -0300

    feat(app): added toast container

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 import Routes from 'routes'
 import Topbar from 'components/Topbar'
 
 import { GlobalStyle, ThemeProvider } from 'theme'
 
+import 'react-toastify/dist/ReactToastify.css'
+
 const App = () => (
   <ThemeProvider>
     <GlobalStyle />
+    <ToastContainer />
     <Router>
       <Topbar />
       <Switch>
