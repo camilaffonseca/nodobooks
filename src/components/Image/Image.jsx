646fec13a2fce73d8cc32da07e6dd65e9d3db34f
camilaffonseca <camilafonseca.ang7@gmail.com>
2020-11-11 22:28:18 -0300

    feat(image): added react-image-appear

---
+++ src/components/Image/Image.jsx
@@ -0,0 +1,40 @@
+import ReactImageAppear from 'react-image-appear'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { space, layout } from 'styled-system'
+
+const Image = ({ src, alt, ...props }) => <ImageComponent draggable={false} src={src} alt={alt} {...props} />
+
+export const LoaderImage = ({ src, alt, ...props }) => (
+  <LoaderImageComponent animation='fadeIn' animationDuration='1s' draggable={false} src={src} alt={alt} {...props} />
+)
+
+const ImageComponent = styled.img`
+  &::selection {
+    background: transparent;
+  }
+
+  ${space}
+  ${layout}
+`
+
+const LoaderImageComponent = styled(ReactImageAppear)`
+  &::selection {
+    background: transparent;
+  }
+
+  ${space}
+  ${layout}
+`
+
+Image.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired
+}
+
+LoaderImage.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired
+}
+
+export default Image
