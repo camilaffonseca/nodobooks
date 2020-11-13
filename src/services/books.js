8000d0af4b9bccd2a690b888f2e944d1429d87a1
camilaffonseca <camilafonseca.ang7@gmail.com>
2020-11-13 06:27:44 -0300

    feat(services): added api routes

--- src/services/books.js
+++ src/services/books.js
@@ -7,3 +7,7 @@ export const getBooks = () => client.get('/books')
 export const getBookById = id => client.get(`/books/${id}`)
 
 export const filterBooksByCategory = category => client.get(`/books?category=${category}`)
+
+export const searchBook = query => client.get(`/books/search?query=${query}`)
+
+export const buyBook = payload => client.post('/buy', payload)
